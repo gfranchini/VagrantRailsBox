be5d4f53ddf17859b85c70d4f4e580e8935d4f65
Giancarlo Franchini <gfranchini88@gmail.com>
2016-10-27 23:28:28 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# A Virtual Machine for Ruby on Rails Core Development
+
+## Introduction
+
+
+This project automates the setup of a development environment for working on a Ruby on Rails project.
+## Requirements
+
+* [VirtualBox](https://www.virtualbox.org)
+
+* [Vagrant](http://vagrantup.com)
+
+## Installs
+
+- Ruby 2.3.1
+- Rails 5.0.0.1
---
+++ bootstrap.sh
@@ -0,0 +1,54 @@
+# The output of all these installation steps is noisy. With this utility
+# the progress report is nice and concise.
+function install {
+    echo installing $1
+    shift
+    apt-get -y install "$@" >/dev/null 2>&1
+}
+
+echo adding swap file
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+
+echo updating package information
+apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
+apt-get -y update >/dev/null 2>&1
+
+install 'development tools' build-essential
+
+install Ruby ruby2.3 ruby2.3-dev
+update-alternatives --set ruby /usr/bin/ruby2.3 >/dev/null 2>&1
+update-alternatives --set gem /usr/bin/gem2.3 >/dev/null 2>&1
+
+echo installing Bundler
+gem install bundler -N >/dev/null 2>&1
+
+install Git git
+git config --global color.ui true
+git config --global user.name "Giancarlo Franchini"
+git config --global user.email "gfranchini88@gmail.com"
+
+echo installing mysql
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password '
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '
+install MySQL mysql-server mysql-client libmysqlclient-dev
+# mysql -uroot -proot <<SQL
+# CREATE USER ''@'localhost';
+# SQL
+
+install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev zlib1g-dev
+install 'Blade dependencies' libncurses5-dev
+install 'ExecJS runtime' nodejs
+
+echo installing rails
+gem install rails 5.0.0.1
+
+
+
+# Needed for docs generation.
+update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
+
+echo 'all set, rock on!'
